a582989a1a9534b66157121abcafbe489e03e527
Richard Johnston <techpowerawaits@outlook.com>
2025-02-15 15:45:26 -0700

    Revert "Make wolf move"
    This reverts commit 9395c559a747ba2fff3306ff1fe66f12882f592c.

--- wolf.gd
+++ wolf.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
-@export var progress_per_frame: float = 0.5
-
 func _ready() -> void:
 	$AnimatedSprite2D.play('walk');
 	
 
-func _physics_process(_delta: float) -> void:
-	get_parent().progress += progress_per_frame;
+func _physics_process(delta: float) -> void:
+	pass
