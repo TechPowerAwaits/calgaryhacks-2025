60699a88454271b2f21fcb6048ba66a30d0931f2
Richard Johnston <techpowerawaits@outlook.com>
2025-02-16 04:39:44 -0700

    Handle saved Wolves in Level 1

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_wolves_death_limit_reached() -> void:
 	get_tree().change_scene_to_file("res://levels/game_over.tscn");
+
+
+func _on_wolves_wolves_gone() -> void:
+	get_tree().change_scene_to_file("res://levels/Level_2.tscn");
