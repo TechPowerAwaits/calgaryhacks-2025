e8ca5eae5124a3cff0539e23c1f3d2c045fff68e
Richard Johnston <techpowerawaits@outlook.com>
2025-02-15 21:49:04 -0700

    Jumping somewhat works now
    Isn't that suprising?

--- wolf.gd
+++ wolf.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 @export var speed := 15;
 @export var jump_wait_time := 1;
 @export var jump_length := 2;
+@export var jump_speed = 20;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,10 +20,12 @@ func _physics_process(delta: float) -> void:
 		var collision := move_and_collide(Vector2.RIGHT * speed * delta);
 		
 		if collision and collision.get_angle() != 0:
+			position.x -= 8;
+			# move_and_collide(Vector2.LEFT * speed * 5 * delta);
 			jump();
 	if perform_jump:
-		apply_central_force(Vector2.UP * 1000);
-		apply_central_force(Vector2.RIGHT * speed);
+		apply_central_force(Vector2.UP * 1020);
+		apply_central_force(Vector2.RIGHT * jump_speed);
 
 
 func jump():
