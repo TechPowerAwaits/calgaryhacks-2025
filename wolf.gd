5702ce562985350a3d635be3e1904eacc712cdeb
Richard Johnston <techpowerawaits@outlook.com>
2025-02-15 21:58:28 -0700

    Prevent wolf from faceplanting
    Backflips are also prohibbited.

--- wolf.gd
+++ wolf.gd
@@ -8,7 +8,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	lock_rotation = true;
 
 var jumping := false;
 var perform_jump := false;
