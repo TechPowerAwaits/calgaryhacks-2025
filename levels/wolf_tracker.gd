2db2db609cdfe1a38af191ce17b1cda37d941ef5
Richard Johnston <techpowerawaits@outlook.com>
2025-02-16 04:34:40 -0700

    Handle saved wolves in WolfTracker

--- levels/wolf_tracker.gd
+++ levels/wolf_tracker.gd
@@ -1,14 +1,27 @@
 extends Node
 
 signal death_limit_reached
+signal wolves_gone
 
 @export var child_death_limit := 1;
+
+var wolf_count := 0;
 
 func _on_child_entered_tree(node: Node) -> void:
 	if node is Wolf:
 		node.dead.connect(_on_child_dead);
+		node.saved.connect(_on_child_saved);
+		wolf_count += 1;
 
 func _on_child_dead():
 	child_death_limit -= 1;
+	wolf_count -= 1;
 	if child_death_limit == 0:
 		emit_signal("death_limit_reached");
+	elif wolf_count == 0:
+		emit_signal("wolves_gone");
+
+func _on_child_saved():
+	wolf_count -= 1;
+	if wolf_count == 0:
+		emit_signal("wolves_gone");
