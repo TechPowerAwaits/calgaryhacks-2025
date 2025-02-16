77b01ec6f597d971d80e7747a2ca51a5e88faeb4
Richard Johnston <techpowerawaits@outlook.com>
2025-02-16 04:52:43 -0700

    Increase safety of scene switching in Level 2

--- levels/level_2.gd
+++ levels/level_2.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_wolves_death_limit_reached() -> void:
-	get_tree().change_scene_to_file("res://levels/game_over.tscn");
+	get_tree().call_deferred("change_scene_to_file", "res://levels/game_over.tscn");
 
 
 func _on_wolves_wolves_gone() -> void:
-	get_tree().change_scene_to_file("res://levels/main_menu.tscn");
+	get_tree().call_deferred("change_scene_to_file", "res://levels/main_menu.tscn");
