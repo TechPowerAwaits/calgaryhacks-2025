6aa308b1f0664c46c44f8de85de030be6ee95a18
Richard Johnston <techpowerawaits@outlook.com>
2025-02-15 21:25:23 -0700

    Improve jumping
    Still doesn't work properly, but progress has been made.

--- wolf.gd
+++ wolf.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
 @export var speed := 15;
-@export var jump_wait_time := 2;
+@export var jump_wait_time := 1;
+@export var jump_length := 2;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,23 +10,26 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 var jumping := false;
+var perform_jump := false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if not jumping:
+	if not (jumping or perform_jump):
 		$AnimatedSprite2D.play("walk");
 		var collision := move_and_collide(Vector2.RIGHT * speed * delta);
 		
-		#if collision and collision.get_local_shape() == $JumpDetector.shape: #and collision.get_angle() == PI/2:
-		#	jump();
+		if collision:
+			jump();
+	if perform_jump:
+		apply_central_force(Vector2.UP * 1000);
 
 
 func jump():
 	jumping = true;
 	$AnimatedSprite2D.play("sit");
-	await get_tree().create_timer(jump_wait_time);
-	###move_and_collide(Vector2(Vector2.LEFT * speed));
+	await get_tree().create_timer(jump_wait_time).timeout;
 	$AnimatedSprite2D.play("jump");
-	###move_and_collide(Vector2(speed, 32));
-	#move_and_collide(Vector2.UP * 250);
+	
 	jumping = false;
+	perform_jump = true;
+	get_tree().create_timer(jump_length).timeout.connect(func (): perform_jump = false);
