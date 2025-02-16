c27a9f474d3d455a6be7fa29e1a77ae3b506bd5d
Richard Johnston <techpowerawaits@outlook.com>
2025-02-16 08:58:25 -0700

    Update FloodedTile to work with Shovel

--- world objects/flooded_tile.gd
+++ world objects/flooded_tile.gd
@@ -1,15 +1,13 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var is_safe := false;
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.get_parent() is CharacterBody2D:
-		area.get_parent().queue_free(); 
+	if area.name == "Shovel":
+		$Sprite2D.texture = load("res://sprites/dirt tile.png");
+		is_safe = true;
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Wolf and not is_safe:
+		body.hurt();
