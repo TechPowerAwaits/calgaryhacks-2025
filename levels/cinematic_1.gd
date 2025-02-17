0e0ab29893d8ff59349b324fc80c5830b8ba0914
Richard Johnston <techpowerawaits@outlook.com>
2025-02-17 16:32:51 -0700

    Allow skipping cinematic
    The length of the cinematic animations were updated.
    
    Fixes #10.

--- levels/cinematic_1.gd
+++ levels/cinematic_1.gd
@@ -1,9 +1,28 @@
 extends ColorRect
 
+enum {INTRO, FADE_OUT}
+
+var state := INTRO;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("intro");
-	await $AnimationPlayer.animation_finished;
-	$AnimationPlayer.play("Outro");
-	await $AnimationPlayer.animation_finished;
-	get_tree().change_scene_to_file("res://levels/Level_1.tscn");
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("skip_cutscene"):
+		_fade_out();
+
+
+func _fade_out():
+	if state != FADE_OUT:
+		state = FADE_OUT;
+		$SkipText.hide();
+		$AnimationPlayer.play("Outro");
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	if state == INTRO:
+		_fade_out();
+	elif state == FADE_OUT:
+		get_tree().change_scene_to_file("res://levels/Level_1.tscn");
