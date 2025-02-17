513fbe2f00cf587a250fdc9271c45c55ebf422b0
Richard Johnston <techpowerawaits@outlook.com>
2025-02-17 15:39:54 -0700

    Redesign Game Over Scene
    Fixes #12.

--- levels/game_over.gd
+++ levels/game_over.gd
@@ -5,5 +5,5 @@ func _on_main_menu_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/main_menu.tscn");
 
 
-func _on_button_pressed() -> void:
+func _on_restart_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/Level_1.tscn");
