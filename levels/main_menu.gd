6bb663174d9aca6b33beb790e4f29b3b89aad27f
Richard Johnston <techpowerawaits@outlook.com>
2025-04-23 19:21:13 -0600

    Hide Exit button for web builds

--- levels/main_menu.gd
+++ levels/main_menu.gd
@@ -1,5 +1,10 @@
 extends TextureRect
 
+func _ready() -> void:
+	if OS.has_feature("web"):
+		$quit.set_deferred("disabled", true);
+		$quit.call_deferred("hide");
+
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/Cinematic1.tscn");
 
