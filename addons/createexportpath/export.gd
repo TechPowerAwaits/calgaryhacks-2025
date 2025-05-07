aafb8d257b25cec6dbfda1b97fc8cfafcb8141e8
Richard Johnston <techpowerawaits@outlook.com>
2025-05-06 21:17:16 -0600

    Implement missing virtual function in CreateExportPath

--- addons/createexportpath/export.gd
+++ addons/createexportpath/export.gd
@@ -1,5 +1,8 @@
 extends EditorExportPlugin
 
+func _get_name() -> String:
+	return "CreateExportPath"
+
 # Purpose: Creates the directories that make up the given path if they don't already exist.
 func _export_begin(_features: PackedStringArray, _is_debug: bool, path: String, _flags: int) -> void:
 	var basePath: String = path.get_base_dir()
